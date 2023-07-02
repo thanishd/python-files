ac42881d5caae7a24c549bb50aa803a68c62b807
thanishd <115917253+thanishd@users.noreply.github.com>
2023-07-03 01:13:05 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,8 @@
+x=int(input("enter the number"))
+y=int(input("enter the number"))
+if x==y:
+    print("the values are equal")
+elif x<y:
+    print("the y value is greater than x value")
+else:
+    print("the x value is greater")
