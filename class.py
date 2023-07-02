1376c5ff858574a9b7f4172ab661adce30cf0ef3
thanishd <115917253+thanishd@users.noreply.github.com>
2023-07-03 01:18:26 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,22 @@
+class addition:
+    def add(self):
+        a=int(input("enter the a value: "))
+        b=int(input("enter the b value: "))
+        c=a+b
+        print(c)
+
+class subtraction:
+    def sub(self):
+        d=int(input("enter the d value: "))
+        e=int(input("enter the e value: "))
+        f=d-e
+        print(f)
+
+obj1=addition()
+obj1.add()
+
+obj2=subtraction()
+obj2.sub()
+
+
+
